8251fbfdf85599c0289221070f8739a08f36a7e6
danguya <daniel.yava16@gmail.com>
2023-09-28 23:24:21 +0100

    importando os componenets no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import "./App.css";
 import Header from "./components/header";
-import './App.css'
+import Card from "./components/card";
+import { CARDS_INFO } from "./utils/data";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="container">
+        <Header />
+        <div className="cards">
+          {CARDS_INFO.map(({ id, description, img, title }) => (
+            <Card id={id} description={description} img={img} title={title} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
